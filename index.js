2ebc33757d3d35b9df3f68740ea64f4c8a15123b
Obiedere Okezie Frank <35150643+okezieobi@users.noreply.github.com>
2022-02-21 14:19:27 +0100

    index file updated

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const input = '(((([{}]))))';
 
 function findBalanced(string = '') {
   if (typeof string !== 'string') throw new TypeError('Argument must be string type');
+  else if (string.length % 2 !== 0) throw new RangeError('String length must be even');
   const array = string.split('');
-  if (array.length % 2 !== 0) throw new RangeError('String length must be even');
   let result;
   array.forEach((element) => {
     if ((element === '(' && array[array.length - 1] === ')') || (element === '[' && array[array.length - 1] === ']') || (element === '{' && array[array.length - 1] === '}')) {
